23d7bd745dfa5480fa859cb6642c1c9ded791a32
Ayotundequest <akinjolequadri03@gmail.com>
2025-07-07 10:13:02 -0700

    wip

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
@@ -9,4 +11,4 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
-}
+};
